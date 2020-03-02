b2f49526cfc86ac12572471e0593026325ee67de
legion91 <58102807+legion91@users.noreply.github.com>
2020-03-02 21:20:46 +0200

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,32 @@
-
+$('.co-founder__partners').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: false,
+    autoplaySpeed: 1000,
+    infinite: true,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
